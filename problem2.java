e18c6c52f25befce8000adbc968ee1ca4740a414
Ayushman SSingh <ayushmansingh7706@gmail.com>
2026-09-10 20:26:21 +0530

    Hollow Rectangle Problem

---
+++ problem2.java
@@ -0,0 +1,31 @@
+/*
+Hollow Rectangle Problem : 
+
+Print : 
+* * * * *
+*       *
+*       *
+* * * * *    4 rows and 5 columns 
+ */
+
+class Problem {
+
+    public static void main(String[] args) {
+
+        int m = 4;
+        int n = 5;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == m || j == 1 || j == n) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                } 
+                }
+                System.out.println();
+            }
+            
+        }
+    }
+
