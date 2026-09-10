4b757cc52f02a39532cf28edfb3fc3ce622ac162
Ayushman SSingh <ayushmansingh7706@gmail.com>
2026-09-10 20:27:25 +0530

    Half Pyramid Problem

---
+++ problem3.java
@@ -0,0 +1,27 @@
+/*
+Half Pyramid Problem : 
+
+Print : 
+* 
+**
+***
+**** 
+ */
+
+class Problem {
+
+    public static void main(String[] args) {
+
+        int m = 4;
+       
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+            
+        }
+    }
+
