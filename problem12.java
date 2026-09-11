69ef73d066115b0780cd23424f12c4b0f59f0d87
Ayushman SSingh <ayushmansingh7706@gmail.com>
2026-09-11 21:49:19 +0530

    Number Pyramid Pattern

---
+++ problem12.java
@@ -0,0 +1,39 @@
+// Advance problem 3: 
+
+// Number Pyramid : 
+/* print:
+    1
+   2 2 
+  3 3 3
+ 4 4 4 4
+5 5 5 5 5 
+ 
+
+*/
+import java.util.Scanner;
+
+class Problem {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows : ");
+        int n = sc.nextInt();
+
+        
+        for (int i = 1; i <= n; i++) {
+            int spaces = n-i;
+
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i+" ");
+            }
+
+            System.out.println();
+        }
+
+        
+    }
+}
